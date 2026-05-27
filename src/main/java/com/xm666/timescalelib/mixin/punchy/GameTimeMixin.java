167f93f570c3f2f74f87be462f04f0b1dde9706e
XM666-Dev <x94732568960@163.com>
2026-05-27 17:31:01 +0800

    Improve mixin class structure Fix LivingEntity walk animation Fix bow animation with punchy

--- src/main/java/com/xm666/timescalelib/mixin/punchy/GameTimeMixin.java
+++ src/main/java/com/xm666/timescalelib/mixin/punchy/GameTimeMixin.java
@@ -1,12 +1,15 @@
 package com.xm666.timescalelib.mixin.punchy;
 
+import com.bawnorton.mixinsquared.TargetHandler;
 import com.xm666.timescalelib.handler.TimeScaleHandler;
 import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.renderer.item.ItemProperties;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import punchy.client.animation.PunchyAnimationManager;
 import punchy.client.state.AttackStateMachine;
 import punchy.client.state.MiningStateMachine;
 import punchy.client.state.SpearStateMachine;
@@ -42,6 +45,23 @@ public class GameTimeMixin {
     private static class UseItemStateMachineMixin {
         @Redirect(method = "currentTick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getGameTime()J"))
         private long redirectGameTime(ClientLevel instance) {
+            return TimeScaleHandler.clientTimer.getTickCount();
+        }
+    }
+
+    @Mixin(PunchyAnimationManager.class)
+    private static class PunchyAnimationManagerMixin {
+        @Redirect(method = "updateUseMeshSuppression", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getGameTime()J"))
+        private static long redirectGameTime(ClientLevel instance) {
+            return TimeScaleHandler.clientTimer.getTickCount();
+        }
+    }
+
+    @Mixin(value = ItemProperties.class, priority = 2000)
+    private static class ItemPropertiesMixin {
+        @TargetHandler(mixin = "punchy.mixin.client.ItemPropertiesMixin", name = "resolveRemainingTicks")
+        @Redirect(method = "@MixinSquared:Handler", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/ClientLevel;getGameTime()J"))
+        private static long redirectGameTime(ClientLevel instance) {
             return TimeScaleHandler.clientTimer.getTickCount();
         }
     }
