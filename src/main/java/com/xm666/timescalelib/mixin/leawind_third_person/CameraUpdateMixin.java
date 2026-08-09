b4d8ecc751325a9cc0bff6dad94cbab28edfb116
XM666-Dev <x94732568960@163.com>
2026-08-09 19:42:50 +0800

    Fix

--- src/main/java/com/xm666/timescalelib/mixin/leawind_third_person/CameraUpdateMixin.java
+++ src/main/java/com/xm666/timescalelib/mixin/leawind_third_person/CameraUpdateMixin.java
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 public class CameraUpdateMixin {
-    @Mixin(ThirdPersonPerspective.class)
+    @Mixin(value = ThirdPersonPerspective.class, remap = false)
     private static class ThirdPersonPerspectiveMixin {
         @WrapOperation(method = "computeCameraState", at = @At(value = "INVOKE", target = "Lio/github/leawind/perspectiveapi/api/PerspectiveContext;partialTicks()F"))
         private static float wrapPartialTick(PerspectiveContext instance, Operation<Float> original, @Local(name = "entity") Entity entity) {
@@ -26,7 +26,7 @@ public class CameraUpdateMixin {
         }
     }
 
-    @Mixin(MinecraftCameraPivotIntegration.class)
+    @Mixin(value = MinecraftCameraPivotIntegration.class, remap = false)
     private static class MinecraftCameraPivotIntegrationMixin {
         @WrapWithCondition(method = "onClientTick", at = @At(value = "INVOKE", target = "Lio/github/leawind/thirdperson/internal/logic/base/pivot/CameraPivotTracker;updateTick(Lorg/joml/Vector3dc;DLio/github/leawind/thirdperson/internal/logic/base/pivot/CameraPivotSmoothing;)Ljava/util/Optional;"))
         private static boolean wrapUpdateTick(CameraPivotTracker instance, Vector3dc target, double deltaSeconds, CameraPivotSmoothing smoothing, @Local(name = "entity") Entity entity) {
