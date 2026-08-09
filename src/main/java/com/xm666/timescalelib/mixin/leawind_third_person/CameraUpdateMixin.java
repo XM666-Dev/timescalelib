18d2eed98fdd916fa2369d7606d089b488618b06
XM666-Dev <x94732568960@163.com>
2026-08-09 19:12:45 +0800

    Fix

--- src/main/java/com/xm666/timescalelib/mixin/leawind_third_person/CameraUpdateMixin.java
+++ src/main/java/com/xm666/timescalelib/mixin/leawind_third_person/CameraUpdateMixin.java
@@ -1,21 +1,19 @@
 package com.xm666.timescalelib.mixin.leawind_third_person;
 
-import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.xm666.timescalelib.handler.TimeScaleHandler;
 import io.github.leawind.perspectiveapi.api.PerspectiveContext;
 import io.github.leawind.thirdperson.internal.logic.base.ThirdPersonPerspective;
-import io.github.leawind.thirdperson.internal.logic.base.camera.CameraInput;
-import io.github.leawind.thirdperson.internal.logic.base.camera.CameraSmoother;
-import io.github.leawind.thirdperson.internal.logic.base.camera.CameraSmoothingParameters;
-import net.minecraft.client.Minecraft;
+import io.github.leawind.thirdperson.internal.logic.base.pivot.CameraPivotSmoothing;
+import io.github.leawind.thirdperson.internal.logic.base.pivot.CameraPivotTracker;
+import io.github.leawind.thirdperson.internal.logic.base.pivot.MinecraftCameraPivotIntegration;
 import net.minecraft.world.entity.Entity;
+import org.joml.Vector3dc;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-
-import java.util.Optional;
 
 public class CameraUpdateMixin {
     @Mixin(ThirdPersonPerspective.class)
@@ -28,13 +26,11 @@ public class CameraUpdateMixin {
         }
     }
 
-    @Mixin(CameraSmoother.class)
-    private static class CameraSmootherMixin {
-        @WrapMethod(method = "update")
-        private Optional<CameraInput> wrapUpdate(CameraInput target, double deltaSeconds, CameraSmoothingParameters smoothing, Operation<Optional<CameraInput>> original) {
-            var mc = Minecraft.getInstance();
-            var entity = mc.getCameraEntity();
-            return TimeScaleHandler.clientTimer.runsTravelling(entity) ? original.call(target, deltaSeconds, smoothing) : Optional.empty();
+    @Mixin(MinecraftCameraPivotIntegration.class)
+    private static class MinecraftCameraPivotIntegrationMixin {
+        @WrapWithCondition(method = "onClientTick", at = @At(value = "INVOKE", target = "Lio/github/leawind/thirdperson/internal/logic/base/pivot/CameraPivotTracker;updateTick(Lorg/joml/Vector3dc;DLio/github/leawind/thirdperson/internal/logic/base/pivot/CameraPivotSmoothing;)Ljava/util/Optional;"))
+        private static boolean wrapUpdateTick(CameraPivotTracker instance, Vector3dc target, double deltaSeconds, CameraPivotSmoothing smoothing, @Local(name = "entity") Entity entity) {
+            return TimeScaleHandler.clientTimer.runsTravelling(entity);
         }
     }
 }
