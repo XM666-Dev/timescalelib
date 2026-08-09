839c1b0aff7e1b23960cde8207b13e932af144df
XM666-Dev <x94732568960@163.com>
2026-08-09 18:59:30 +0800

    Add compatiblity support for latest version of Leawind's Third Person

--- src/main/java/com/xm666/timescalelib/mixin/leawind_third_person/CameraUpdateMixin.java
+++ src/main/java/com/xm666/timescalelib/mixin/leawind_third_person/CameraUpdateMixin.java
@@ -1,24 +1,26 @@
 package com.xm666.timescalelib.mixin.leawind_third_person;
 
-import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import com.xm666.timescalelib.handler.TimeScaleHandler;
 import io.github.leawind.perspectiveapi.api.PerspectiveContext;
-import io.github.leawind.thirdperson.internal.logic.base.MinecraftClientIntegration;
 import io.github.leawind.thirdperson.internal.logic.base.ThirdPersonPerspective;
-import io.github.leawind.thirdperson.internal.logic.base.camera.CameraPivotSmoother;
+import io.github.leawind.thirdperson.internal.logic.base.camera.CameraInput;
+import io.github.leawind.thirdperson.internal.logic.base.camera.CameraSmoother;
 import io.github.leawind.thirdperson.internal.logic.base.camera.CameraSmoothingParameters;
+import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.Entity;
-import org.joml.Vector3dc;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Optional;
 
 public class CameraUpdateMixin {
     @Mixin(ThirdPersonPerspective.class)
     private static class ThirdPersonPerspectiveMixin {
-        @WrapOperation(method = "applyCameraState", at = @At(value = "INVOKE", target = "Lio/github/leawind/perspectiveapi/api/PerspectiveContext;partialTicks()F"))
+        @WrapOperation(method = "computeCameraState", at = @At(value = "INVOKE", target = "Lio/github/leawind/perspectiveapi/api/PerspectiveContext;partialTicks()F"))
         private static float wrapPartialTick(PerspectiveContext instance, Operation<Float> original, @Local(name = "entity") Entity entity) {
             return TimeScaleHandler.isEntityEnforceableFrozen(entity)
                     ? TimeScaleHandler.getScalablePartialTick(!TimeScaleHandler.isEntityOriginalFrozen(entity))
@@ -26,11 +28,13 @@ public class CameraUpdateMixin {
         }
     }
 
-    @Mixin(MinecraftClientIntegration.class)
-    private static class MinecraftClientIntegrationMixin {
-        @WrapWithCondition(method = "onClientTick", at = @At(value = "INVOKE", target = "Lio/github/leawind/thirdperson/internal/logic/base/camera/CameraPivotSmoother;updateTick(Lorg/joml/Vector3dc;DLio/github/leawind/thirdperson/internal/logic/base/camera/CameraSmoothingParameters;)Ljava/util/Optional;"))
-        private static boolean wrapUpdateTick(CameraPivotSmoother instance, Vector3dc target, double deltaSeconds, CameraSmoothingParameters smoothing, @Local(name = "cameraEntity") Entity cameraEntity) {
-            return TimeScaleHandler.clientTimer.runsTravelling(cameraEntity);
+    @Mixin(CameraSmoother.class)
+    private static class CameraSmootherMixin {
+        @WrapMethod(method = "update")
+        private Optional<CameraInput> wrapUpdate(CameraInput target, double deltaSeconds, CameraSmoothingParameters smoothing, Operation<Optional<CameraInput>> original) {
+            var mc = Minecraft.getInstance();
+            var entity = mc.getCameraEntity();
+            return TimeScaleHandler.clientTimer.runsTravelling(entity) ? original.call(target, deltaSeconds, smoothing) : Optional.empty();
         }
     }
 }
